5a1e96ae73b4b03df4504ea7e00809e47e03920e
dounya-m <mellouki.dounya@gmail.com>
2022-02-19 11:16:50 +0100

    last aupdate

--- booking.js
+++ booking.js
@@ -1,11 +1,14 @@
 const types = document.getElementById('Types');
 const trans = document.getElementsByName('transmission')
-const printTotal = document.getElementById('total')
 const manual = document.getElementById('manual');
 const auto = document.getElementById('auto');
-const fuel=document.getElementsByName('boit');
+const fuel = document.getElementsByName('boit');
 const days  = document.getElementById('days');
-
+const elec = document.getElementById('electrique')
+const hybride = document.getElementById('hybride')
+const essence = document.getElementById('essence')
+const diesel = document.getElementById('diesel')
+const printTotal = document.getElementById('total')
 
 var final = 0;
 var total = 0;
@@ -14,13 +17,20 @@ var transValue = 0;
 types.addEventListener('change',()=>{
 
     switch (types.value) {
-        case 'moto': total = 10;manual.disabled = true;auto.disabled = true;break;
-        case 'citadine': total = 12; auto.disabled= true;manual.disabled = false; break;
-        case 'compact': total = 14; auto.disabled= true;manual.disabled = false;break;
-        case 'berline': total = 20;manual.disabled = true;auto.disabled= false;break;
-        case 'utilitaire': total = 16; auto.disabled= true;manual.disabled = false;break;
-        case 'egine': total = 900; auto.disabled= true;manual.disabled = false;break;
-        case 'camion': total = 250;manual.disabled = true;auto.disabled= false;break;
+        case 'moto': total = 10;manual.disabled = true;auto.disabled = true;
+        elec.disabled = false;hybride.disabled=true;essence.disabled=false;diesel.disabled=true;break;
+        case 'citadine': total = 12; auto.disabled= true;manual.disabled = false;
+        elec.disabled = false;hybride.disabled=false;essence.disabled=false;diesel.disabled=false;break;
+        case 'compact': total = 14; auto.disabled= true;manual.disabled = false;
+        elec.disabled = true;hybride.disabled=false;essence.disabled=false;diesel.disabled=false;break;
+        case 'berline': total = 20;manual.disabled = true;auto.disabled= false
+        elec.disabled = true;hybride.disabled=false;essence.disabled=false;diesel.disabled=false;break;
+        case 'utilitaire': total = 16; auto.disabled= true;manual.disabled = false;
+        elec.disabled = true;hybride.disabled=true;essence.disabled=true;diesel.disabled=false;break;
+        case 'egine': total = 900; auto.disabled= true;manual.disabled = false;
+        elec.disabled = true;hybride.disabled=true;essence.disabled=false;diesel.disabled=false;break;
+        case 'camion': total = 250;manual.disabled = true;auto.disabled= false;
+        elec.disabled = true;hybride.disabled=true;essence.disabled=true;diesel.disabled=false;break;
     }
 })
 
@@ -35,9 +45,15 @@ fuel.forEach((e)=>{
         fuelValue = e.value
     })
 })
-
 function prices(){
-    final = (total + total*fuelValue + total*transValue ) * days.value
-    printTotal.innerHTML = final
+
+    if (days.value < 0 ){
+        final = 0
+    }
+    else{
+        console.log ("work Too");
+        final = (total + total*fuelValue + total*transValue ) * days.value
+}
+        printTotal.innerHTML = final;
 
 }
